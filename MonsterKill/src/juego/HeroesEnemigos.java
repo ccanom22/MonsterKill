516acf1792e2d6b5c09211b240c6deae356a8837
kimeraxxx <59649388+kimeraxxx@users.noreply.github.com>
2020-01-22 13:43:43 +0100

    Update HeroesEnemigos.java
    CREO QUE YA

--- MonsterKill/src/juego/HeroesEnemigos.java
+++ MonsterKill/src/juego/HeroesEnemigos.java
@@ -1,5 +1,26 @@
 package juego;
 
 public class HeroesEnemigos {
+ private Heroes heroes;
+	private byte idEnemigos;
 
+	public HeroEnemigos(Heroes heroes, byte idEnemigos) {
+		super();
+		this.heroes = heroes;
+		this.idEnemigos = idEnemigos;
+	}
+
+	private byte getIdEnemigos() {
+		return idEnemigos;
+	}
+
+	private void setIdEnemigos(byte idEnemigos) {
+		this.idEnemigos = idEnemigos;
+	}
+
+	private Heroes getHeroes() {
+		return heroes;
+	}
+	
+  
 }
