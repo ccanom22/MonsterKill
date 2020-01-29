d6f249c511192f2f200ef2b9fb3f2d75b4e9b767
Alemew <59649239+Alemew@users.noreply.github.com>
2020-01-29 12:11:32 +0100

    Update HeroesEnemigos.java

--- MonsterKill/src/juego/HeroesEnemigos.java
+++ MonsterKill/src/juego/HeroesEnemigos.java
@@ -2,6 +2,13 @@ package juego;
 public class HeroeEnemigo {
 	private Heroe enemigo;
 	private byte idEnemigos;
+	
+	public void crearEnemigo(){
+		String[] nombre = {"Dragon", "Ogro", "Duende"};
+		int[] vida = {500,300,175};
+		int[] ataque = {90,70,50};
+		int[] curacion = {70,60,50};
+	}
 
 	public HeroeEnemigo(Heroe heroe, byte idEnemigos) {
 		super();
